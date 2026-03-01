4b117736c4e0170e7ac1a657ecdff8e5fa06493d
Kushagra Kishore <kushagra@Kushagras-MacBook-Air-127.local>
2026-03-02 05:27:49 +0530

    Order Struct Created

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+enum class Side {
+    Buy,
+    Sell
+};
+
+struct Order {
+    // Price,Quantity,Buy or Sell, ID 
+    int64_t id;
+    uint16_t quantity;
+    Side side;
+    int16_t price;
+};
+
+
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    
+
+    return 0;
+}
